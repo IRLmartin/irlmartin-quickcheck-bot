0ce6bc246995d461be78e9e96377493cb0e887a1
IRLmartin <martinnealon1998@gmail.com>
2025-05-31 21:33:36 +0100

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
 
+# Any extra setup commands go here
+
+echo "Running post-install tasks..."
+
+# Example: just sleep for 3 seconds (you can remove or replace this)
+sleep 3
+
+echo "Post-install tasks completed."
